f10b8531b18c9ebc190c458b0d15a0d74bbdc099
Elias Berg <eliasroberg@gmail.com>
2020-12-19 15:08:16 -0600

    Uploaded enhancements
    Major changes; completely changed style, changed TLP to be more generic, and added various functionality and quality-of-life improvements.

--- Timelogger/logfuncs.js
+++ Timelogger/logfuncs.js
@@ -3,17 +3,41 @@
 // And he's quick with a joke or to light up your smoke
 // But there's someplace that he'd rather be
 
-var tlps = {
-  "12": "Design",
-  "16": "Design Review",
-  "13": "Development",
-  "14": "PQA",
-  "50337": "Development Support (Internal)",
-  "13279": "QAN Investigation"
+// REVISIONS:
+//  10/18/20 - Cleanup
+
+var quickCodes = {
+  "12": ["DES", "Design"],
+  "16": ["DES REV", "Design Review"],
+  "13": ["DEV", "Development"],
+  "14": ["PQA", "PQA"],
+  "50337": ["DEV SUPP", "Dev. Support (Internal)"],
+  "13279": ["QAN INV", "QAN Investigation"],
+  "39509": ["PERF", "Perf. Testing"],
+  "11457": ["RN REV", "Release Note Review"],
+  "20273": ["DEV SET", "Dev. System Setup"],
+  "119": ["WRKPLN", "Workplan"],
+  "3048": ["TRVL", "Travel Time"],
+  "949": ["FLR SUP", "Go-Live Floor Support"],
+  "154": ["TRN", "Training"],
+  "808": ["MENT", "Mentoring"],
+  "7091": ["MTNG", "Meetings"],
+  "2527": ["1TO1", "Team Member 1-to-1"]
 };
 
-function quickAdd(ev)
+// If there's anything in the grid and the day isn't finished, make sure they actually wanna close the tab!
+window.onbeforeunload = function(ev) {
+  return true;
+}
+
+/**
+ * Click event handler for the quick Code plus buttons.
+ * Adds a Code to the grid with no description.
+ */
+function quickAddCode(ev)
 {
+  if (dayCalled) { return; }
+  
   if (!ev || !ev.target) { return; }
   
   var row = ev.target;
@@ -22,97 +46,196 @@ function quickAdd(ev)
     row = row.parentNode;
   }
   
-  saveTLP(row.childNodes[1].innerText, row.childNodes[2].innerText, "");
+  var codeID = row.childNodes[1].innerText;
+  var desc = getQuickCode(codeID, true);
+  
+  saveCode(codeID, desc, "");
+  endBreak();
 }
 
-function loadCommonTLPs()
+/**
+ *
+ */
+function processDomContentLoaded()
 {
-  var table = document.getElementById("commontlps");
+  loadQuickCodesIntoDOM();
+  
+  var bufferField = document.getElementById("buffer");
+  if (bufferField)
+  {
+    bufferField.addEventListener("keydown", validateNumeric);
+  }
+}
+
+/**
+ *
+ */
+function validateNumeric(ev)
+{
+  if (!ev || !ev.key)
+    return;
+  
+  var bufferField = ev.target;
+  var numericText = bufferField.value + ev.key;
+  var numericValue = parseFloat(numericText);
+  if (isNaN(numericValue))
+  {
+    alert("Invalid key entered; start buffer should be an integer or decimal value.");
+    ev.preventDefault();
+    return;
+  }
+}
+
+/**
+ *
+ */
+function getStartBufferValue()
+{
+  var bufferField = document.getElementById("buffer");
+  if (bufferField)
+  {
+    var numericValue = parseFloat(bufferField.value);
+    if (!isNaN(numericValue))
+    {
+      return numericValue;
+    }
+  }
+  return 0;
+}
+
+/**
+ * Load-process run function to create a table of quick-add Codes.
+ */
+function loadQuickCodesIntoDOM()
+{
+  var table = document.getElementById("commoncodes");
   if (!table) { return; }
   
-  var tlp, tr, td;
-  for (tlp in tlps)
+  var codeID, tr, td;
+  for (codeID in quickCodes)
   {
     tr = document.createElement("tr");
     td = document.createElement("td");
     td.className = "add";
     td.innerHTML = plusSVG();
-    td.addEventListener("click", quickAdd);
+    td.addEventListener("click", quickAddCode);
     tr.appendChild(td);
+    
     td = document.createElement("td");
-    td.innerText = tlp;
+    td.style.fontSize = "12px";
+    td.innerText = codeID;
     tr.appendChild(td);
+    
     td = document.createElement("td");
-    td.innerText = tlps[tlp];
+    td.style.fontSize = "12px";
+    td.innerText = getQuickCode(codeID);
     tr.appendChild(td);
+    
     table.appendChild(tr);
   }
 }
 
+/**
+ *
+ */
+function getQuickCode(codeID, fullName)
+{
+  var codeObj = quickCodes[codeID];
+  if (!codeObj) return "";
+  
+  if (fullName)
+    return codeObj[1];
+  else
+    return codeObj[0];
+}
+
+/**
+ * The raw SVG markup for a green plus sign.
+ */
 function plusSVG()
 {
   return "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 160 160'><g><polygon points='140,60 100,60 100,20 60,20 60,60 20,60 20,100 60,100 60,140 100,140 100,100 140,100 '></polygon></g></svg>"
 }
 
-document.addEventListener("DOMContentLoaded", loadCommonTLPs);
-
-var DATE;
-var curtime = 0;
-var subtotal = 0;
-var tlpMap = [];
-
+/**
+ *
+ */
 function saveLine()
 {
-  var curtlp = document.getElementById("tlp");
+  var curcode = document.getElementById("code");
   var curcomm = document.getElementById("comm");
-  var tlp = curtlp.value;
+  var codeID = curcode.value;
   var comm = curcomm.value;
   
-  if (tlp === "") // quit if not all data entered
+  var desc = getQuickCode(codeID, true);
+  
+  if (codeID === "" && comm === "") // quit if not all data entered
+  {
+    alert("Either a Code or Comment is needed!");
     return;
+  }
+    
+  saveCode(codeID, desc, comm);
   
-  var desc = tlps[tlp] || "";
-  saveTLP(tlp, desc, comm);
-  
-  curtlp.value = "";
+  curcode.value = "";
   curcomm.value = "";
+  
+  endBreak();
 }
 
-function saveTLP(tlp, desc, comm)
+/**
+ * Helper function to:
+ * - Add a new line to the grid
+ * - Save the last Code to the background data structure
+ */
+function saveCode(code, desc, comm, isBreak)
 {
-  var cur = dateInfo();
-  if (curtime > 0) // fill the last cell
-    updateLastCell(cur);
-  curtime = cur;
+  code = code || "-";
+  
+  var timeEnded = getNumericResetTime();
+  if (timeStarted > 0) // fill the last cell
+    updateLastCell(timeEnded);
+  timeStarted = timeEnded;
   
   var table = document.getElementById("timetable");
   var row = table.insertRow(-1);
   
-  // tlp cell
-  var cell = row.insertCell();
-  cell.innerHTML = tlp;
-  cell.id = "lasttlp";
-  cell.className = "erb_cell";
-  
-  // description cell
-  cell = row.insertCell();
-  cell.innerHTML = desc;
-  cell.className = "erb_cell";
-  
-  // comments cell
-  cell = row.insertCell();
-  cell.innerHTML = comm;
-  cell.className = "erb_cell";
+  if (isBreak)
+  {
+    var cell = row.insertCell();
+    cell.colSpan = "3";
+    cell.classList.add("break");
+    cell.innerHTML = "Break";
+    cell.className = "erb_cell";
+    cell.id = "lastcode";
+  }
+  else
+  {
+    // code cell
+    var cell = row.insertCell();
+    cell.innerHTML = code;
+    cell.id = "lastcode";
+    cell.className = "erb_cell";
+
+    // description cell
+    cell = row.insertCell();
+    cell.innerHTML = desc;
+    cell.className = "erb_cell";
+
+    // comments cell
+    cell = row.insertCell();
+    var commInput = document.createElement("input");
+    commInput.dataset.code = code; // Custom property
+    commInput.classList.add("codeComm");
+    commInput.classList.add("disableme");
+    commInput.value = comm;
+    cell.appendChild(commInput);
+    cell.className = "erb_cell";
+  }
   
   // start time cell
   cell = row.insertCell();
-  cell.innerHTML = DATE.getHours() + ":" + DATE.getMinutes();
-  cell.className = "erb_cell";
-  
-  // end time cell
-  cell = row.insertCell();
-  cell.id = "endtime";
-  cell.innerHTML = 0;
+  cell.innerHTML = getLocaleTimeString();
   cell.className = "erb_cell";
   
   // time spent cell
@@ -128,80 +251,105 @@ function saveTLP(tlp, desc, comm)
   cell.className = "erb_cell";
 }
 
-function dateInfo()
+/**
+ * Helper function to reset the timer to 'now' and then get the current numeric base-10 time
+ * for easier calculations of time spent doing tasks.
+ * 
+ * For example, 5:30PM returns 17.5
+ *
+ * So then 5:30PM - 2:15PM -> 17.5 - 14.25 = 3.25 hours!
+ * See how easy that was?
+ */
+function getNumericResetTime()
 {
-  DATE = new Date();
-  return DATE.getHours() + (DATE.getMinutes() / 60);
+  timerDateObj = new Date();
+  return timerDateObj.getHours() + (timerDateObj.getMinutes() / 60);
 }
 
+/**
+ * Wrapper around getting the locale time string of the global date
+ * object for display to the end-user.
+ */
+function getLocaleTimeString()
+{
+  return timerDateObj.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+}
+
+/**
+ *
+ */
 function finish()
 {
-  var cur = dateInfo();
-  updateLastCell(cur);
+  var atTime = document.getElementById("atTime");
+  atTime.innerText = "@ " + getLocaleTimeString();
+  
+  var timeEnded = getNumericResetTime();
+  updateLastCell(timeEnded);
   
   var totals = document.getElementById("totals");
-  totals.style.display = 'block';
+  totals.classList.remove("nodisp");
+  
+  // Get all comments
+  var codeComments = aggregateCodeComments();
   
   // Populate table totals
   var row,cell;
   var table = document.getElementById("totaltable");
   var total = 0;
-  for (var tlp in tlpMap)
+  for (var codeID in codeMap)
   {
     row = table.insertRow();
-    addERBCell(row, tlp);
-    addERBCell(row, tlps[tlp] || "");
-    addERBCell(row, "Not Implemented");
-    var time = tlpMap[tlp];
-    addERBCell(row, time);
+    addTotalCell(row, codeID);
+    addTotalCell(row, getQuickCode(codeID, true));
+    addTotalCell(row, codeComments[codeID]);
+    var time = codeMap[codeID];
+    addTotalCell(row, time);
     total = total + time;
   }
   
+  // Start buffer
+  var startBuffer = getStartBufferValue();
+  total = total + startBuffer;
+  
+  row = table.insertRow();
+  var cell = addTotalCell(row, "Start Buffer");
+  cell.colSpan = "3";
+  addTotalCell(row, startBuffer);
+  
   // Total total at the bottom of the total table
   row = table.insertRow();
-  var cell = addERBCell(row, "Total");
+  var cell = addTotalCell(row, "Grand Total");
   cell.colSpan = "3";
-  addERBCell(row, total);
+  cell.style.fontWeight = 600;
+  addTotalCell(row, total);
   
   // Disable the controls
+  dayCalled = true;
   var disables = document.getElementsByClassName("disableme");
   for(var i = 0; i < disables.length; i++)
     disables[i].disabled = true;
 }
 
-function addERBCell(row, data)
+/**
+ *
+ */
+function addTotalCell(row, data)
 {
   var cell = row.insertCell();
   cell.innerHTML = data;
-  cell.className = "erb_cell";
   return cell; // in case we want to do anything with the cell
 }
 
-function getFormattedTime()
-{
-  var hour = "" + DATE.getHours();
-  if (hour.length === 1) hour = "0" + hour;
-  
-  var minute = "" + DATE.getMinutes();
-  if (minute.length === 1) minute = "0" + minute;
-  
-  return hour + ":" + minute;
-}
-
-function updateLastCell(cur)
+/**
+ *
+ */
+function updateLastCell(timeEnded)
 {
   var cell, rounded;
-  
-  // end time cell
-  cell = document.getElementById("endtime");
-  if (!cell)
-    return;
-  cell.innerHTML = getFormattedTime();
-  cell.id = "";
 
   // time spent cell
   cell = document.getElementById("timespent");
-  rounded = round(cur - curtime);
+  rounded = round(timeEnded - timeStarted);
   cell.innerHTML = rounded;
   cell.id = "";
   
@@ -212,21 +360,30 @@ function updateLastCell(cur)
   cell.id = "";
   
   // update data array
-  cell = document.getElementById("lasttlp");
-  var tlp = cell.innerHTML;
-  addToTotals(tlp, rounded);
+  cell = document.getElementById("lastcode");
+  var code = cell.innerHTML;
+  addToTotals(code, rounded);
   cell.id = "";
 }
 
-function addToTotals(tlp, rounded)
+/**
+ *
+ */
+function addToTotals(code, rounded)
 {
-  // see if the TLP exists
-  if (!tlpMap[tlp])
-    tlpMap[tlp] = rounded;
+  if (code === "Break")
+    return;
+  
+  // see if the Code exists
+  if (!codeMap[code])
+    codeMap[code] = rounded;
   else
-    tlpMap[tlp] += rounded;
+    codeMap[code] += rounded;
 }
 
+/**
+ *
+ */
 function round(num)
 {
   var rounded = num;
@@ -235,3 +392,100 @@ function round(num)
   rounded = rounded / 100;
   return rounded;
 }
+
+/**
+ *
+ */
+function aggregateCodeComments()
+{
+  var codeComms = {};
+  var inputs = document.getElementsByClassName("codeComm");
+  
+  for (var i in inputs)
+  {
+    var input = inputs[i];
+    if (!input || !input.dataset) continue;
+    var code = input.dataset.code;
+    if (!code) continue;
+    
+    if (!codeComms[code]) codeComms[code] = input.value;
+    else codeComms[code] = codeComms[code] + "<br/>" + input.value;
+  }
+  
+  return codeComms;
+}
+
+///////////////////////////
+///////////////////////////
+// BREAK START/STOP
+///////////////////////////
+///////////////////////////
+
+/**
+ *
+ */
+function pause()
+{
+  saveCode("","","",true);  
+  
+  // start break
+  var btnPause = document.getElementById("pause");
+  btnPause.setAttribute("disabled", true);
+  
+  var btnUnpause = document.getElementById("unpause");
+  btnUnpause.removeAttribute("disabled");
+}
+
+/**
+ *
+ */
+function unpause()
+{
+  getPreBreakCode();
+  endBreak();
+}
+
+/**
+ *
+ */
+function getPreBreakCode()
+{
+  var table = document.getElementById("timetable");
+  if (table.rows.length < 2) { return; } // Sanity check
+  var lastRow = table.rows[table.rows.length - 2];
+  var codeCell = lastRow.cells[0];
+  
+  var code = codeCell.innerText;
+  var desc = getQuickCode(code, true);
+  saveCode(code);
+}
+
+/**
+ *
+ */
+function endBreak()
+{  
+  var btnUnpause = document.getElementById("unpause");
+  if (btnUnpause.hasAttribute("disabled"))
+  {
+    return;
+  }  
+  btnUnpause.setAttribute("disabled", true);
+  
+  var btnPause = document.getElementById("pause");
+  btnPause.removeAttribute("disabled");
+}
+
+///////////////////////////
+///////////////////////////
+// SCRIPT START
+///////////////////////////
+///////////////////////////
+
+document.addEventListener("DOMContentLoaded", processDomContentLoaded);
+
+var timerDateObj;
+var dayCalled = false;
+var timeStarted = 0;
+var subtotal = 0;
+var codeMap = [];
