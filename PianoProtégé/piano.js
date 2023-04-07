051a6176d875471f90b3b1824fe654eb5a195927
elrobe <eliasroberg@gmail.com>
2023-04-07 17:50:00 -0500

    Added Piano practicing page (still in development)

---
+++ PianoProtégé/piano.js
@@ -0,0 +1,140 @@
+class PianoKey {
+  constructor(x, y, w, h, isBlack, note) {
+    this.X = x;
+    this.Y = y;
+    this.W = w;
+    this.H = h;
+    this.IsBlack = !!isBlack;
+    this.Fill = this.IsBlack ? "black" : "white";
+    this.Audio = new Audio(`assets/sounds/${note}.m4a`);
+    this.Audio.currentTime = 0.35;
+  }
+
+  pointInKey = (x, y) => {
+    if (x < this.X) return false;
+    if (y < this.Y) return false;
+    if (x > this.X + this.W) return false;
+    if (y > this.Y + this.H) return false;
+    return true;
+  }
+
+  setDown = (isDown) => {
+    if (isDown) {
+      this.Fill = "gray";
+      this.Audio.play();
+    } else {
+      this.Fill = this.IsBlack ? "black" : "white";
+      this.Audio.pause();
+      this.Audio.currentTime = 0.35;
+    }
+  }
+
+  paintKey = (context) => {
+    context.strokeStyle = "black";
+    context.fillStyle = this.Fill;
+    context.fillRect(this.X, this.Y, this.W, this.H);
+    context.strokeRect(this.X, this.Y, this.W, this.H);
+  }
+}
+
+const paintAll = (keys, ctx) => {
+  for (let key of keys) {
+    key.paintKey(ctx);
+  }
+}
+
+// Separate collection of black keys and white keys to make it easier
+// to detect which is being pressed; since the black overlap the white,
+// we can check them first, and short-circuit the checking.
+let whiteKeys = [];
+let blackKeys = [];
+let pressedKey = null;
+
+const notes = ["C", "D", "E", "F", "G", "A", "B"];
+
+const canvas = document.getElementById("piano");
+const ctx = canvas.getContext("2d");
+
+const paintPiano = () => {
+  whiteKeys = [];
+  blackKeys = [];
+
+  const container = document.getElementById("bottom");
+  canvas.width = container.offsetWidth;
+  canvas.height = container.offsetHeight;
+  
+  const width = canvas.width - 1;
+  const height = canvas.height - 1;
+  
+  // The body of the piano
+  ctx.lineWidth = 2.5;
+  
+  // Now for each white key (C, D, E, F, G, A, and B)
+  const keyW = (width / 7) - 1;
+  for (let i = 0; i < 7; i++) {
+    const k = new PianoKey((keyW * i) + 1, 1, keyW, height - 1, false, notes[i] + "4");
+    whiteKeys.push(k);
+  }
+  
+  // Now for the black keys (C#, D#, F#, G#, and A#)
+  const midW = (keyW / 3) * 2;
+  for (let i = 0; i < 6; i++) {
+    if (i === 2) continue;
+    const k = new PianoKey(midW + (keyW * i) + (keyW / 6), 1, keyW / 3, height * 0.75, true);
+    blackKeys.push(k);
+  }
+  
+  paintAll(whiteKeys, ctx);
+  paintAll(blackKeys, ctx);
+}
+
+window.addEventListener("resize", () => {
+  paintPiano();
+})
+
+canvas.addEventListener("pointerdown", (event) => {
+  console.log(event.offsetX + "," + event.offsetY);
+  pressedKey = null;
+  for (const key of blackKeys) {
+    if (key.pointInKey(event.offsetX, event.offsetY)) {
+      pressedKey = key;
+      break;
+    }
+  }
+  if (!pressedKey) {
+    for (const key of whiteKeys) {
+      if (key.pointInKey(event.offsetX, event.offsetY)) {
+        pressedKey = key;
+        break;
+      }
+    }
+  }
+  if (!pressedKey) return;
+  pressedKey.setDown(true);
+  paintAll(whiteKeys, ctx);
+  paintAll(blackKeys, ctx);
+});
+
+canvas.addEventListener("pointerup", (event) => {
+  pressedKey.setDown(false);
+  pressedKey = null;
+  paintAll(whiteKeys, ctx);
+  paintAll(blackKeys, ctx);
+});
+
+// Script Start
+paintPiano();
+
+setInterval(() => {
+  console.log("interval...");
+  const note = document.getElementById("note");
+  if (note.parentElement !== document.body) {
+    note.parentElement.remove(note);
+    document.body.appendChild(note);
+    note.top = 100;
+  }
+  if (note.style.left === "0px") {
+    note.style.left = document.body.width + "px";
+  }
+  note.style.left = (parseInt(note.style.left) - 10) + "px";
+}, 250);
